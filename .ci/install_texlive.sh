0a117758f8775f8a7218c91b360b6de48c9f8bc6
Tomas Baca <klaxalk@gmail.com>
2020-05-06 22:17:45 +0200

    updated travis

--- .ci/install_texlive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
-
-sudo apt -y install texlive texlive-latex-base texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra
